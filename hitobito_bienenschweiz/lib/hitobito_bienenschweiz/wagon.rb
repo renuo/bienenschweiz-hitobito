123065071561e9b6403bdb211f924618ac1c6f04
mike <michael.gerber@renuo.ch>
2026-04-29 15:50:17 +0200

    add :salutation, :hive_count, :honey_yield, :export_to_website to people and display canton

--- hitobito_bienenschweiz/lib/hitobito_bienenschweiz/wagon.rb
+++ hitobito_bienenschweiz/lib/hitobito_bienenschweiz/wagon.rb
@@ -22,6 +22,8 @@ module HitobitoBienenschweiz
     config.to_prepare do
       # extend application classes here
       Group.include Bienenschweiz::Group
+
+      PeopleController.permitted_attrs += [:salutation, :hive_count, :honey_yield, :export_to_website]
     end
 
     initializer "bienenschweiz.add_settings" do |_app|
