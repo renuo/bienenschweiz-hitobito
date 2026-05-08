93325ffab66518b3077a7cda209b8439c821a829
mike <michael.gerber@renuo.ch>
2026-05-08 14:38:27 +0200

    improve import scripts

--- bin/dump_db.sh
+++ bin/dump_db.sh
@@ -9,5 +9,5 @@ popd
 pg_dump hit_bienenschweiz_dev --no-acl --no-owner --schema-only --clean > dump.sql
 pg_dump hit_bienenschweiz_dev --data-only >> dump.sql
 
-eche "Now import"
+echo "Now import"
 echo "psql -h hitobito-develop.8a5926d.db.postgres.nineapis.ch -d 1c62958_d4d57b3 -U 1c62958_d4d57b3 < dump.sql"
