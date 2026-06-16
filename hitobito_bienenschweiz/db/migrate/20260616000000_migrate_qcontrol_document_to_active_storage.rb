102804e136831797227e308b33a189d7e66dfbc3
Mike <400227+firemind@users.noreply.github.com>
2026-06-16 11:50:21 +0200

    implement document upload on qcontrol (#71)

---
+++ hitobito_bienenschweiz/db/migrate/20260616000000_migrate_qcontrol_document_to_active_storage.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+#  Copyright (c) 2012-2026, BienenSchweiz. This file is part of
+#  hitobito_bienenschweiz and licensed under the Affero General Public License version 3
+#  or later. See the COPYING file at the top-level directory or at
+#  https://github.com/renuo/bienenschweiz-hitobito/tree/develop/hitobito_bienenschweiz.
+
+class MigrateQcontrolDocumentToActiveStorage < ActiveRecord::Migration[8.0]
+  def change
+    remove_column :qcontrols, :document, :string
+    remove_column :qcontrols, :content_type, :string
+    remove_column :qcontrols, :file_size, :integer
+  end
+end
