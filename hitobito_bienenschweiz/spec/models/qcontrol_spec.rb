e5a5a16c694879cb9f8cce6d701589f876d5e508
Mike <400227+firemind@users.noreply.github.com>
2026-06-19 14:58:04 +0200

    Update the beekeeper role based on qcontrol (#74)
    * update the beekeeper role based on qcontrol
    
    * remove duplicate spec

--- hitobito_bienenschweiz/spec/models/qcontrol_spec.rb
+++ hitobito_bienenschweiz/spec/models/qcontrol_spec.rb
@@ -8,7 +8,7 @@
 require "spec_helper"
 
 describe Qcontrol do
-  let(:group) { Fabricate(:sektion) }
+  let(:group) { groups(:aarau_und_umgebung) }
   let(:person) { Fabricate(:person) }
 
   describe "#previous_qcontrol" do
@@ -61,6 +61,48 @@ describe Qcontrol do
 
       it "gives the previous one by control_date" do
         expect(qcontrol.previous_qcontrol).to eq(previous2)
+      end
+    end
+  end
+
+  describe "#update_beekeeper_role" do
+    let(:person) { Fabricate(:beekeeper, group_id: group.id) }
+    subject(:role) { person.roles.first }
+
+    let(:no_control_reason) { :no_reason }
+    let(:qcontrol) do
+      Fabricate(:qcontrol, person: person, group: group,
+        control_date: Date.new(2026, 5, 1),
+        control_state:, no_control_reason:)
+    end
+
+    context "when control_state is not_passed" do
+      let(:control_state) { "not_passed" }
+
+      it "updates the end_on of the role to 20 days from now" do
+        expect {
+          qcontrol
+        }.to change { role.reload.end_on }.to(Time.zone.today + 20.days)
+      end
+    end
+
+    context "when control_state is passed" do
+      let(:control_state) { "passed" }
+
+      it "does not update end_on" do
+        expect {
+          qcontrol
+        }.not_to change { role.reload.end_on }
+      end
+
+      context "when there is a no_control reason" do
+        let(:no_control_reason) { :beekeeper_deceased }
+
+        it "updates the end_on of the role to today" do
+          expect {
+            qcontrol
+          }.to change { role.reload.end_on }.to(Time.zone.today)
+        end
       end
     end
   end
