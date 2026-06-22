3a70214ff1275df2990531efa16578ce1529205c
mike <michael.gerber@renuo.ch>
2026-06-22 17:16:11 +0200

    exclude delayed_jobs table data from dump

--- bin/dump_db.sh
+++ bin/dump_db.sh
@@ -9,7 +9,7 @@ pushd hitobito
 popd
 
 pg_dump hit_bienenschweiz_dev --no-acl --no-owner --schema-only --clean > dump.sql
-pg_dump hit_bienenschweiz_dev --data-only >> dump.sql
+pg_dump hit_bienenschweiz_dev --data-only --exclude-table=delayed_jobs >> dump.sql
 
 echo "Now import"
 echo "psql -h hitobito-develop.8a5926d.db.postgres.nineapis.ch -d 1c62958_d4d57b3 -U 1c62958_d4d57b3 < dump.sql"
