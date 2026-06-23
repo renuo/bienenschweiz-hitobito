d32b8a0f7b55cecb8654c673fcce1fc8a84ca2d7
mike <michael.gerber@renuo.ch>
2026-06-23 16:01:44 +0200

    linter fix

--- hitobito_bienenschweiz/app/models/supervision_type.rb
+++ hitobito_bienenschweiz/app/models/supervision_type.rb
@@ -5,7 +5,6 @@
 #  or later. See the COPYING file at the top-level directory or at
 #  https://github.com/renuo/bienenschweiz-hitobito/tree/develop/hitobito_bienenschweiz.
 
-
 class SupervisionType < ApplicationRecord
   has_many :supervisions, dependent: :restrict_with_error
   validates :name, presence: true, uniqueness: true
