abd2396be1da032ef31e33715be3d8204296ddc7
mike <michael.gerber@renuo.ch>
2026-06-26 10:30:24 +0200

    add copyright

--- hitobito_bienenschweiz/db/migrate/20260626080514_add_canceled_to_events.rb
+++ hitobito_bienenschweiz/db/migrate/20260626080514_add_canceled_to_events.rb
@@ -1,3 +1,11 @@
+# frozen_string_literal: true
+
+#  Copyright (c) 2012-2026, BienenSchweiz. This file is part of
+#  hitobito_bienenschweiz and licensed under the Affero General Public License version 3
+#  or later. See the COPYING file at the top-level directory or at
+#  https://github.com/renuo/bienenschweiz-hitobito/tree/develop/hitobito_bienenschweiz.
+
+
 class AddCanceledToEvents < ActiveRecord::Migration[8.0]
   def change
     add_column :events, :canceled, :boolean, null: false, default: false
