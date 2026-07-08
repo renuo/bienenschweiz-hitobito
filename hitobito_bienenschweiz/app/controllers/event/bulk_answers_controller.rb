c74f4bc404c103ba89b11bbaa551548c42e6c8d7
Mike <400227+firemind@users.noreply.github.com>
2026-07-08 13:43:56 +0200

    implement bulk edit of participation answers (#107)
    * implement bulk edit of participation answers
    
    * only show admin questions in bulk edit
    
    * Update hitobito_bienenschweiz/app/controllers/event/bulk_answers_controller.rb
    
    Co-authored-by: Chris <76159444+hunchr@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Chris <76159444+hunchr@users.noreply.github.com>

---
+++ hitobito_bienenschweiz/app/controllers/event/bulk_answers_controller.rb
@@ -0,0 +1,56 @@
+# frozen_string_literal: true
+
+# Copyright (c) 2026. BienenSchweiz. This file is part of
+# hitobito_bienenschweiz and licensed under the Affero General Public License version 3
+# or later. See the COPYING file at the top-level directory or at
+# https://github.com/renuo/bienenschweiz-hitobito/tree/develop/hitobito_bienenschweiz
+
+class Event::BulkAnswersController < ApplicationController
+  before_action :load_group_and_event
+  before_action :authorize_action
+  prepend Nestable
+  self.nesting = Group, Event
+
+  decorates :event
+  def edit
+    @questions = @event.questions.where(admin: true).list.includes(:translations)
+    @participations = load_participations
+  end
+
+  def update
+    valid_ids = event_answer_ids
+    params[:answers]&.each do |answer_id_str, answer_attrs|
+      answer_id = answer_id_str.to_i
+      next unless valid_ids.include?(answer_id)
+
+      Event::Answer.find(answer_id).update(answer: answer_attrs[:answer])
+    end
+    redirect_to edit_group_event_bulk_answers_path(@group, @event), notice: t(".success")
+  end
+
+  private
+
+  def load_group_and_event
+    @group = Group.find(params[:group_id])
+    @event = Event.find(params[:event_id])
+  end
+
+  def authorize_action
+    authorize! :update, @event
+  end
+
+  def load_participations
+    @event.participations
+      .includes(answers: {question: :translations}, participant: [])
+      .sort_by { |p| p.person&.full_name.to_s }
+  end
+
+  def event_answer_ids
+    Set.new(
+      Event::Answer
+        .joins(:participation)
+        .where(event_participations: {event_id: @event.id})
+        .ids
+    )
+  end
+end
---
+++ hitobito_bienenschweiz/spec/requests/event/bulk_answers_controller_spec.rb
@@ -0,0 +1,116 @@
+# frozen_string_literal: true
+
+# Copyright (c) 2026. BienenSchweiz. This file is part of
+# hitobito_bienenschweiz and licensed under the Affero General Public License version 3
+# or later. See the COPYING file at the top-level directory or at
+# https://github.com/renuo/bienenschweiz-hitobito/tree/develop/hitobito_bienenschweiz
+
+require "spec_helper"
+
+RSpec.describe Event::BulkAnswersController, type: :request do
+  let(:group) { groups(:root) }
+  let(:admin) { people(:admin) }
+  let(:event) { Fabricate(:event, groups: [group]) }
+  let(:question) { Fabricate(:event_question, event: event, question: "Allergien?", admin: true) }
+  let(:participant) { Fabricate(:person) }
+  let!(:participation) do
+    Fabricate(:event_participation, event: event, participant: participant, active: true)
+  end
+
+  before do
+    roles(:admin)
+    sign_in(admin)
+    question
+  end
+
+  let(:answer) { participation.answers.find_by(question: question) }
+
+  describe "GET #edit" do
+    it "renders successfully" do
+      get edit_group_event_bulk_answers_path(group, event)
+      expect(response).to have_http_status(:ok)
+    end
+
+    it "shows the question text" do
+      get edit_group_event_bulk_answers_path(group, event)
+      expect(response.body).to include("Allergien?")
+    end
+
+    it "shows the participant name" do
+      get edit_group_event_bulk_answers_path(group, event)
+      expect(response.body).to include(participant.full_name)
+    end
+
+    context "question visibility" do
+      let!(:non_admin_question) do
+        Fabricate(:event_question, event: event, question: "Diätvorschriften?", admin: false)
+      end
+
+      it "shows admin questions" do
+        get edit_group_event_bulk_answers_path(group, event)
+        expect(response.body).to include("Allergien?")
+      end
+
+      it "does not show non-admin questions" do
+        get edit_group_event_bulk_answers_path(group, event)
+        expect(response.body).not_to include("Diätvorschriften?")
+      end
+    end
+
+    context "as unauthorized person" do
+      let(:other_person) { Fabricate(:person) }
+
+      before { sign_in(other_person) }
+
+      it "raises CanCan::AccessDenied" do
+        expect do
+          get edit_group_event_bulk_answers_path(group, event)
+        end.to raise_error(CanCan::AccessDenied)
+      end
+    end
+  end
+
+  describe "participations index" do
+    it "shows the bulk edit button for admin" do
+      get group_event_participations_path(group, event)
+      expect(response).to have_http_status(:ok)
+      expect(response.body).to include(edit_group_event_bulk_answers_path(group, event))
+      expect(response.body).to include("Administrationsangaben bearbeiten")
+    end
+  end
+
+  describe "PATCH #update" do
+    it "updates the text answer and redirects" do
+      patch group_event_bulk_answers_path(group, event),
+        params: {answers: {answer.id.to_s => {answer: "Keine"}}}
+      expect(answer.reload.answer).to eq("Keine")
+      expect(response).to redirect_to(edit_group_event_bulk_answers_path(group, event))
+    end
+
+    it "ignores answer ids not belonging to this event" do
+      other_event = Fabricate(:event, groups: [group])
+      other_question = Fabricate(:event_question, event: other_event, question: "Andere Frage?")
+      other_participant = Fabricate(:person)
+      other_participation = Fabricate(:event_participation,
+        event: other_event, participant: other_participant, active: true)
+      other_answer = other_participation.answers.find_by(question: other_question)
+
+      patch group_event_bulk_answers_path(group, event),
+        params: {answers: {other_answer.id.to_s => {answer: "Hack"}}}
+
+      expect(other_answer.reload.answer).not_to eq("Hack")
+    end
+
+    context "as unauthorized person" do
+      let(:other_person) { Fabricate(:person) }
+
+      before { sign_in(other_person) }
+
+      it "raises CanCan::AccessDenied" do
+        expect do
+          patch group_event_bulk_answers_path(group, event), params: {answers: {}}
+        end.to raise_error(CanCan::AccessDenied)
+      end
+    end
+  end
+end
