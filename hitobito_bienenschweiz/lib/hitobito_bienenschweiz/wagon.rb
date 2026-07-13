e5d6370ddcb33d17789219dce720b4f5ff818156
mike <michael.gerber@renuo.ch>
2026-07-13 17:07:45 +0200

    add extra specs to increase coverage

--- hitobito_bienenschweiz/lib/hitobito_bienenschweiz/wagon.rb
+++ hitobito_bienenschweiz/lib/hitobito_bienenschweiz/wagon.rb
@@ -113,7 +113,6 @@ module HitobitoBienenschweiz
     def seed_fixtures
       # :nocov:
       fixtures = root.join("db", "seeds")
-      # :nocov:
       ENV["NO_ENV"] ? [fixtures] : [fixtures, File.join(fixtures, Rails.env)] # rubocop:disable Rails/EnvironmentVariableAccess -- This is initialization
       # :nocov:
     end
